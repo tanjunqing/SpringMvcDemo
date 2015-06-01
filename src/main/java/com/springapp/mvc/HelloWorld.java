8b000ee1afa85e9161c2347f7f8bccaa0e049d1b
tanjunqing <woshiren2>
2015-06-01 10:22:42 +0800

    格式化代码

--- src/main/java/com/springapp/mvc/HelloWorld.java
+++ src/main/java/com/springapp/mvc/HelloWorld.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloWorld {
     @RequestMapping("helloWorld.acs")
-    public String helloWorld(){
+    public String helloWorld() {
         System.out.println("HelloWorld");
         return "HelloWorld";
     }
